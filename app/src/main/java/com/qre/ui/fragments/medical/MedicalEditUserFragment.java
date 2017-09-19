a509f1856e983c03eebda14644f00420f5527475
Pablo <pablopallocchi@gmail.com>
2017-09-18 22:55:04 -0300

    Navigation drawer and fragments

---
+++ app/src/main/java/com/qre/ui/fragments/medical/MedicalEditUserFragment.java
@@ -0,0 +1,13 @@
+package com.qre.ui.fragments.medical;
+
+import com.qre.R;
+import com.qre.ui.fragments.BaseFragment;
+
+public class MedicalEditUserFragment extends BaseFragment {
+
+    @Override
+    protected int getLayout() {
+        return R.layout.fragment_medical_edit_user;
+    }
+
+}
---
+++ app/src/main/java/com/qre/ui/fragments/user/UserManageQRFragment.java
@@ -0,0 +1,13 @@
+package com.qre.ui.fragments.user;
+
+import com.qre.R;
+import com.qre.ui.fragments.BaseFragment;
+
+public class UserManageQRFragment extends BaseFragment {
+
+    @Override
+    protected int getLayout() {
+        return R.layout.fragment_user_manage_qr;
+    }
+
+}
