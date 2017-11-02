f5ef63f9133a8d78508cca546c83efdbcdb55269
fjaite <federico.jaite@gmail.com>
2017-11-02 11:15:35 -0300

    Notificacion en primer plano

--- app/src/main/java/com/qre/services/firebase/QREMessagingService.java
+++ app/src/main/java/com/qre/services/firebase/QREMessagingService.java
@@ -1,6 +1,34 @@
 package com.qre.services.firebase;
 
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.qre.R;
+
+import java.util.Map;
 
 public class QREMessagingService extends FirebaseMessagingService {
+
+    private static final int M_NOTIFICATION_ID = 1;
+
+    @Override
+    public void onMessageReceived(final RemoteMessage remoteMessage) {
+        final Map<String, String> data = remoteMessage.getData();
+
+        if (data != null && data.containsKey("title") && data.containsKey("text")) {
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(this)
+                            .setSmallIcon(R.drawable.ic_notif_logo)
+                            .setContentTitle(data.get("title"))
+                            .setAutoCancel(true)
+                            .setContentText(data.get("text"));
+            final NotificationManager mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            if (mNotifyMgr != null) {
+                mNotifyMgr.notify(M_NOTIFICATION_ID, mBuilder.build());
+            }
+        }
+
+    }
 }
