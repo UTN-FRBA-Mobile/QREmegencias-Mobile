9318332aac1e674f05290e627484f11e1abd80ed
Pablo <pablopallocchi@gmail.com>
2017-11-12 20:02:40 -0300

    Fragment para editar los datos de emergencia del paciente

--- app/src/main/java/com/qre/injection/ServiceComponent.java
+++ app/src/main/java/com/qre/injection/ServiceComponent.java
@@ -14,6 +14,7 @@ import com.qre.ui.activities.MedicalClinicalHistoryViewActivity;
 import com.qre.ui.activities.SeeMoreActivity;
 import com.qre.ui.activities.TemporalCodeActivity;
 import com.qre.ui.fragments.ProfileFragment;
+import com.qre.ui.fragments.medical.MedicalEmergencyDataFragment;
 import com.qre.ui.fragments.user.UserClinicalHistoryFragment;
 import com.qre.ui.fragments.user.UserEmergencyDataFragment;
 import com.qre.ui.fragments.user.UserManageQRFragment;
@@ -42,6 +43,8 @@ public interface ServiceComponent {
     void inject(final UserManageQRFragment userManageQRFragment);
 
     void inject(final UserEmergencyDataFragment userEmergencyDataFragment);
+
+    void inject(final MedicalEmergencyDataFragment medicalEmergencyDataFragment);
 
     void inject(final UserSignedQRFragment userSignedQRFragment);
 
